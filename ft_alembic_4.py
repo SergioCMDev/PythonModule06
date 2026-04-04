110c2a57ebb1a5a8c0085c3737d4a773d0da9333
sergiocm <sergiocm@github.com>
2026-04-05 00:27:36 +0200

    Init

---
+++ ft_alembic_4.py
@@ -0,0 +1,9 @@
+import alchemy
+print("=== Alembic 4 ===")
+print("Accessing the alchemy module using 'import alchemy'")
+print("Testing create_air: Air element created")
+print(alchemy.create_air())
+print("Now show that not all functions can be reach")
+print("This will raise an exception!")
+
+print(alchemy.create_earth())
