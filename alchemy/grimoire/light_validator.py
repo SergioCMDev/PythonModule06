3f39a6bb73801d875a071a5853f2de6c3503abd9
sergiocm <sergiocm@github.com>
2026-04-05 20:01:57 +0200

    Añadidos nuevos archivos de ejercicios

---
+++ alchemy/grimoire/light_validator.py
@@ -0,0 +1,4 @@
+from light_spellbook import light_spell_allowed_ingredients
+
+def validate_ingredients(ingredients: str) -> str:
+	return "VALID"
---
+++ ft_transmutation_2.py
@@ -0,0 +1,5 @@
+import alchemy
+
+print("=== Transmutation 2 ===")
+print("Import alchemy module only")
+print(alchemy.transmutation.recipes.lead_to_gold())
